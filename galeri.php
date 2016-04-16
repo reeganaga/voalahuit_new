16cbe74b5c08ddea7dd32a9d8f94dcde7a350306
sabiqxs <sabiqxs@gmail.com>
2016-04-16 10:02:04 +0700

    loop for show image

--- galeri.php
+++ galeri.php
@@ -3,7 +3,16 @@
 </div>
 <div class="row">
 	<div class="col-md-12 ">
-		<div class="view fourth-effect animated fadeInUp galeri">
+	<?php 
+	for ($i=1; $i <= 8; $i++) { 
+		echo "<div class='view fourth-effect animated fadeInUp galeri'>";
+		echo "<a href='#'><img src='images/galeri/gambar-$i.jpg' class='img-responsive' /></a>";
+		echo "<div class='mask'></div>";
+		echo "</div>";
+	}		
+	 ?>
+
+		<!-- <div class="view fourth-effect animated fadeInUp galeri">
 			<a href="#"><img src="images/galeri/gambar-1.jpg" class="img-responsive" /></a>
 			<div class="mask"></div>
 		</div>
@@ -34,6 +43,7 @@
 		<div class="view fourth-effect animated fadeInUp galeri">
 			<a href="#"><img src="images/galeri/gambar-8.jpg" class="img-responsive" /></a>
 			<div class="mask"></div>
-		</div>
+		</div> -->
+		
 	</div>
 </div>
