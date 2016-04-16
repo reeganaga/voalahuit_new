71b9672df7b664e566edd80024bacd9cfc2dd8e6
reeganaga <rega.blank@gmail.com>
2016-04-16 20:37:07 +0700

    konfirmasi,detail_transaksi,

---
+++ detail_transaksi.php
@@ -0,0 +1,85 @@
+		<div class="row body-content">
+ 			<div class="col-md-4 animated zoomInLeft" id="">
+ 				<div class="box-content">
+ 					<div class="box-header">Desain</div>
+ 					<div class="box-body">
+						<div class="papercraft-jadi">
+			        		<div id="base_paper"><img src="images/base_clear.PNG"></div>
+					        <div id="rambut"><img src="images/rambut/h01.png"></div>
+					        <!-- <div id="kulit"><img src="images/kulit/h01.png"></div> -->
+					        <div id="mata"><img src="images/mata/e01.png"></div>
+					        <div id="hidung"><img src="images/hidung/n01.png"></div>
+					        <div id="mulut"><img src="images/mulut/m01.png"></div>
+					        <div id="alis"><img src="images/alis/eb01.png"></div>
+					        <div id="baju"><img src="images/baju/cl01.png"></div>
+					        <div id="celana"><img src="images/celana/pn01.png"></div>
+					        <div id="sepatu"><img src="images/sepatu/sh01.png"></div>
+					        <div id="topi"><img src="images/topi/hat01.png"></div>
+					        <div id="kacamata"><img src="images/kacamata/eyg01.png"></div>	
+				        </div>						
+ 					</div>
+ 				</div>
+ 			</div>
+ 			<div class="col-md-8 animated zoomInRight" id="">
+ 				<div class="box-content">
+ 					<div class="box-header">Data Diri</div>
+ 					<div class="box-body">
+						<table class="table table-hover">
+							<tr>
+								<td width="30%">No Transaksi</td>
+								<td width="70%"><strong style="color: red;">#721</strong></td>
+							</tr>
+							<tr>
+								<td width="30%">Nama Pembeli</td>
+								<td width="70%">rega</td>
+							</tr>
+							<tr>
+								<td width="30%">Alamat Dikirim</td>
+								<td width="70%">Rega</td>
+							</tr>
+							<tr>
+								<td width="30%">Email</td>
+								<td width="70%">Rega.blank@gmail.com</td>
+							</tr>
+							<tr>
+								<td width="30%">HP</td>
+								<td width="70%">085</td>
+							</tr>
+							<tr>
+								<td width="30%">Ucapan</td>
+								<td width="70%">HBD sayangku</td>
+							</tr>
+							<tr>
+								<td>&nbsp;</td>
+								<td>&nbsp;</td>
+							</tr>
+							<tr>
+								<td width="30%">Kode Token</td>
+								<td width="70%">
+									<div class="isi_token" style="color: red;">R32gYz</div><small> * simpan kode ini untuk cek status </small>
+								</td>
+							</tr>
+							<tr>
+								<td width="30%">Status</td>
+								<td width="70%"><div class="btn btn-warning btn-flat">Pending</div></td>
+							</tr>
+							<tr>
+								<td width="30%">Total yang harus dibayar</td>
+								<td width="70%"><strong>Rp 100.000,00</strong></td>
+							</tr>
+						</table>
+						<form class="form">
+							<div class="form-group">
+								<label class="label-control">Upload Bukti Pembayaran</label>
+								<input type="file" class=""></input>
+							</div>
+							<div class="form-group">
+								<input type="submit" class="btn btn-primary btn-flat" value="Konfirmasi"></input>
+							</div>
+						</form>
+ 					</div>
+ 				</div>
+ 				
+ 			</div>
+ 		</div>
+					
--- konfirmasi.php
+++ konfirmasi.php
@@ -1,20 +1,15 @@
 		<div class="row body-content">
  			<div class="col-md-4 col-md-offset-4 animated zoomInLeft" id="">
  				<!-- <h3 class="text-center">Konfirmasi</h3> -->
- 						<div class="row">
- 							<div class="col-md-6" style="padding-right: 0px;"><button id="konfirmasi" class="btn btn-primary btn-flat form-control">Konfirmasi</button></div>
- 							<div class="col-md-6" style="padding-left: 0px;"><button id="cek" class="btn btn-success btn-flat form-control">Cek Your Gift</button></div>
- 						</div>
- 						<div class="divider"></div>
+
  				<div class="box-content">
  					<div class="box-body">
- 						
 						<form id="formkonfirmasi" class="form" action="">
 							<div class="form-group">
-								<input type="text" required="" class="form-control btn-flat" placeholder="Masukkan kode transaksi "></input>
+								<input type="text" required="" class="form-control btn-flat" placeholder="Masukkan Kode Token "></input>
 							</div>
 							<div class="form-group">
-								<input type="submit" required="" class="form-control btn btn-primary btn-flat" value="Konfirmasi"></input>
+								<input type="submit" required="" class="form-control btn btn-primary btn-flat" value="Konfirmasi & Cek Status"></input>
 							</div>
 						</form>
 						<form id="formcek" class="form" action="" style="display: none;">
@@ -24,25 +19,17 @@
 							<div class="form-group">
 								<input type="submit" class="form-control btn btn-success btn-flat" value="Check Your Gift"></input>
 							</div>
-						</form>						
+						</form>
+ 						<i><small>*Masukkan kode token yang anda peroleh dari email untuk melakukan konfirmasi dan melihat status pemesanan anda</small></i>
+
  					</div>
  				</div>
  			</div>
  		</div>
 
 <script type="text/javascript">
-	/*$("#konfirmasi").click()(function(){
-		$("#formkonfirmasi").fadeTo(1000,1)
-		$("#formcek").fadeTo(1000,0.0)
-		$("#formcek").hide()
-	})
-	$("#cek").click()(function(){
-		$("#formkonfirmasi").fadeTo(1000,0.0)
-		$("#formcek").fadeTo(1000,1)
-		$("#formkonfirmasi").hide()
-	})*/
 
-	$("#konfirmasi").click(function(){
+/*	$("#konfirmasi").click(function(){
 	    $("#formcek").fadeTo(1000, 0.0);
 	    $("#formcek").hide();
 	    $("#formkonfirmasi").fadeTo(1000, 1);
@@ -53,5 +40,5 @@
 	    $("#formkonfirmasi").fadeTo(1000, 0.0);
 	    $("#formkonfirmasi").hide();
 
-	});
+	});*/
 </script>
